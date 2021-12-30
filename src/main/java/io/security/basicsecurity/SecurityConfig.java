2c6e324810b581ecf19d880c454c140ee902b6d4
Timo <developer.timo@gmail.com>
2021-12-30 16:30:20 +0900

    [스프링 시큐리티 주요 아키텍처 이해] 2) 필터 초기화와 다중 보안 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,25 +1,37 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/admin/**")
                 .authorizeRequests()
-                .anyRequest().authenticated();
-        http
-                .formLogin()
-        ;
+                .anyRequest().authenticated()
+        .and()
+                .httpBasic();
+    }
+}
 
-        //CSRF 토큰 비활성화(csrf() 명시 안해주면 기본으로 활성화됨)
+@Configuration
+@Order(1)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
     }
 }
